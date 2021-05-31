13ecd8559e7c74598fc167fdbae2fd037e1951e1
Methmi Nugawela <methmi.nuga@gmail.com>
2021-05-31 11:55:56 +0530

    Built initial front-end  app structure

---
+++ front-end/src/components/atoms/sessions/SessionCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Container, Typography} from "@material-ui/core"
+
+const SessionCard = () => {
+    return(
+        <Container>
+            <Typography variant="h6">A session card</Typography>
+        </Container>
+    )
+}
+
+export default SessionCard;
