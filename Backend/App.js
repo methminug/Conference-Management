586be928b8e9e130ea28b3a5f6b2ace750fad000
shalithacode <shalithalakruwan1998@gmail.com>
2021-06-01 16:02:15 +0530

    Database connection created

---
+++ Backend/App.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose
+   .connect(
+      "mongodb+srv://shalitha:JaQSeqkhtO7235CB@conference-data.vet1s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+   )
+   .then((result) => {
+      app.listen(3000);
+      console.log("Connected");
+   })
+   .catch((err) => {
+      console.log(err);
+   });
