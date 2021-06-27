1435eee9cf6be8e574941203bbd63569f170cae4
shalithacode <shalithalakruwan1998@gmail.com>
2021-06-27 21:32:30 +0530

    line change

--- Backend/App.js
+++ Backend/App.js
@@ -4,16 +4,14 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 mongoose
-   .connect(
-      "mongodb+srv://shalitha:JaQSeqkhtO7235CB@conference-data.vet1s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-      { useNewUrlParser: true, useUnifiedTopology: true }
-   )
-   .then((result) => {
-      app.listen(3000);
-      console.log("Connected");
-   })
-   .catch((err) => {
-      console.log(err);
-   });
+    .connect(
+        "mongodb+srv://shalitha:JaQSeqkhtO7235CB@conference-data.vet1s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then((result) => {
+        app.listen(3000);
+        console.log("Connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
