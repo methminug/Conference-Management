59358f6904b6d3425eae6707f9536cdc57272252
shalithacode <shalithalakruwan1998@gmail.com>
2021-06-29 18:09:12 +0530

    AdminDashboard created

---
+++ front-end/src/components/Admin/AdminDashboard.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import Count from "./Count";
+import Barchart from "./Barchart";
+import Table from "./Table";
+import "./style/admin.css";
+
+const AdminDashboard = () => {
+    const [aCount, setACount] = useState(0);
+    const [rCount, setRCount] = useState(0);
+    const [pCount, setPCount] = useState(0);
+    const [researchPapers, setresearchPapers] = useState([]);
+    const [workshopPapers, setworkshopPapers] = useState([]);
+
+    useEffect(() => {
+        const resAttendee = fetch(`http://localhost:3000/admin/attendeeCount`);
+        const attendeeData = resAttendee.text();
+
+        const resResearcher = fetch(
+            `http://localhost:3000/admin/researchersCount`
+        );
+        const researcherData = resResearcher.text();
+
+        const resPresenter = fetch(
+            `http://localhost:3000/admin/presentersCount`
+        );
+        const presenterData = resPresenter.text();
+
+        const resResearchpapers = fetch(`http://localhost:3000/publication`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const Researchpaper = resResearchpapers.json();
+
+        const resWorkshoppapers = fetch(`http://localhost:3000/workshop`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const Workshoppaper = resWorkshoppapers.json();
+
+        setresearchPapers(Researchpaper.publications);
+        setworkshopPapers(Workshoppaper.workshops);
+
+        if (presenterData != null) {
+            setACount(attendeeData);
+            setRCount(researcherData);
+            setPCount(presenterData);
+        }
+    }, []);
+
+    return (
+        <div style={{ marginBottom: "100px" }}>
+            <Count attendee={aCount} researcher={rCount} presenter={pCount} />
+            <Barchart
+                ResearchPapers={researchPapers}
+                WorkshopPapers={workshopPapers}
+            />
+            <div className="researchTable">
+                <Table
+                    Tablecontent={researchPapers}
+                    topic={"Research Papers"}
+                />
+            </div>
+            <div className="workshopsTable">
+                <Table Tablecontent={workshopPapers} topic={"Workshops"} />
+            </div>
+        </div>
+    );
+};
+
+export default AdminDashboard;
